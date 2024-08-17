2204792566cd28673f5993c99efcb34ec81c7016
ShahidSandhu <sandhulive@gmail.com>
2024-08-17 18:51:01 +0500

    deployed removed error

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { cartReducer } from './CartSlice';
+import CartReducer  from './CartSlice';
 // import { addItem , removeItem, updateQuantity } from './CartSlice';
 
  const store = configureStore({
     reducer: {
-        cart: cartReducer,
+        cart: CartReducer,
     },
 });
 export default store
